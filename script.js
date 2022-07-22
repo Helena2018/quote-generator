e04b3de655e920c8ee8455825ef48991dfeb2f83
Helena2018 <helena2020liu@gmail.com>
2022-07-22 15:53:38 +1200

    grab info from DOM

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const quoteAuthor = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-button')
+
 let apiQuotes = []
 
 // New Quote
 function newQuote(){
-  // Get Random Quote From API Quotes
+  // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-  console.log(quote)
+
+  console.log(quote.text)
 }
 
 // Get Quotes From API
